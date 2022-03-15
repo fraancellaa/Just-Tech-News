f0496cb785addfde473e38c288e7a39d43e20f59
Francella Aguilar <francellaaguilar@Francellas-MBP.attlocal.net>
2022-03-15 16:00:02 -0400

    adding new files

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const routes = require('./routes');
+const sequelize = require('./config/connection');
+const router = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const PORT = process.env.PORT || 3001;
